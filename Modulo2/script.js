5de034679f19212137d496402c18ef0454cf2a25
José Ulombe <jose.ulombe@hotmail.com>
2020-06-23 08:34:00 +0100

    UPDATE file

--- Modulo2/script.js
+++ Modulo2/script.js
@@ -2,6 +2,7 @@
 
 const modalOverlay = document.querySelector('.modal-overlay');
 const cards = document.querySelectorAll('.card')
+const modal = document.querySelector('.modal')
 
 
 for (let card of cards) {
@@ -17,8 +18,15 @@ for (let card of cards) {
 document.querySelector('.close-modal').addEventListener("click", function() {
     modalOverlay.classList.remove("active")
     modalOverlay.querySelector("iframe").src = "" // remove o video na modal quando fechado.
+    modal.classList.remove('maximize')
+
 })
 
-
-
+document
+.querySelector('.maximize-modal')
+.addEventListener("click", function() {
+    if(!modal.classList.contains('maximize')){
+        modal.classList.add('maximize')
+    }
+})
 
