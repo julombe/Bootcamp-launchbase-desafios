a7f77a98495b0cacee3e2c3f9377988b8d0ee17a
José Ulombe <jose.ulombe@hotmail.com>
2020-07-20 01:06:42 +0100

    Upload file

---
+++ Modulo3/script.js
@@ -0,0 +1,32 @@
+// manipulando os cards
+
+const modalOverlay = document.querySelector('.modal-overlay');
+const cards = document.querySelectorAll('.card')
+const modal = document.querySelector('.modal')
+
+
+for (let card of cards) {
+    card.addEventListener("click", function() {
+        const cursoId = card.getAttribute("id"); // pega o id do video no html e faz com que abra na modal cada video clicado
+        modalOverlay.classList.add('active')
+        
+        modalOverlay.querySelector("iframe").src = `https://rocketseat.com.br/${cursoId}` // Pegando o iframe
+    })
+}
+
+// botão para fechar a modal
+document.querySelector('.close-modal').addEventListener("click", function() {
+    modalOverlay.classList.remove("active")
+    modalOverlay.querySelector("iframe").src = "" // remove o video na modal quando fechado.
+    modal.classList.remove('maximize')
+
+})
+
+document
+.querySelector('.maximize-modal')
+.addEventListener("click", function() {
+    if(!modal.classList.contains('maximize')){
+        modal.classList.add('maximize')
+    }
+})
+
