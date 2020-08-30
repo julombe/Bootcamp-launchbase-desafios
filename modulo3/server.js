d222fbd6a7d496a5a3d6b942a1033d786251bcad
José Ulombe <jose.ulombe@hotmail.com>
2020-08-30 17:30:17 +0100

    Add server.js

---
+++ modulo3/server.js
@@ -0,0 +1,29 @@
+
+const express = require('express')
+const nunjucks = require('nunjucks')
+
+const server = express()
+
+//chamando o css
+server.use(express.static('public'))
+
+//configure template engine(nunjucks)
+
+server.set("view engine", "html")
+nunjucks.configure("views", {
+    express: server
+})
+
+//create route
+
+server.get("/", function(req, res) {
+    return res.render("cursos")
+})
+
+server.get("/about", function(req, res) {
+    return res.render("about")
+})
+
+server.listen(5000, function() {
+    console.log("server is running")
+})
