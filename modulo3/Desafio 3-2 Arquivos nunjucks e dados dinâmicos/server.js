7927b5bdd1387ced97a622e9ad54191dddf1c469
José Ulombe <jose.ulombe@hotmail.com>
2020-09-18 23:02:27 +0100

    Add file

---
+++ modulo3/Desafio 3-2 Arquivos nunjucks e dados dinâmicos/server.js
@@ -0,0 +1,35 @@
+
+const express = require('express')
+const nunjucks = require('nunjucks')
+
+const server = express()
+
+//chamando o css
+server.use(express.static('public'))
+
+//configure template engine(nunjucks)
+
+server.set("view engine", "njk")
+nunjucks.configure("views", {
+    express: server
+})
+
+//creating routes
+
+server.get("/", function(req, res) {
+    return res.render("courses")
+})
+
+
+server.get("/about", function(req, res) {
+    return res.render("about")
+})
+
+
+server.use(function(req, res) {
+    res.status(404).render("not-found");
+  });
+
+server.listen(5000, function() {
+    console.log("server is running")
+})
