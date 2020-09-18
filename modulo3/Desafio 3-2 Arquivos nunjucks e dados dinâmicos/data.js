dc9284826c90e01a05246d21204665de2b2a44a4
José Ulombe <jose.ulombe@hotmail.com>
2020-09-18 23:22:21 +0100

    Add file

--- modulo3/Desafio 3-2 Arquivos nunjucks e dados dinâmicos/data.js
+++ modulo3/Desafio 3-2 Arquivos nunjucks e dados dinâmicos/data.js
@@ -2,5 +2,9 @@
 module.exports = [
     {
         id: "starter",
+        img: "https://storage.googleapis.com/golden-wind/bootcamp-launchbase/assets/starter.svg",
+        title: "",
+        module: "5 módulos",
+        price: "Gratuito"
     }
 ]
