fbc24044abc3279f2583df055d0e42e1c09ffa49
José Ulombe <jose.ulombe@hotmail.com>
2020-09-18 23:46:23 +0100

    Add file

--- modulo3/Desafio 3-2 Arquivos nunjucks e dados dinâmicos/data.js
+++ modulo3/Desafio 3-2 Arquivos nunjucks e dados dinâmicos/data.js
@@ -6,5 +6,19 @@ module.exports = [
         description: "<strong>Torne-se um programador desejado</strong> no mercado com esses cursos gratuitos",
         module: "5 módulos",
         price: "Gratuito"
+    },
+    {
+        id: "launchbase",
+        img: "https://storage.googleapis.com/golden-wind/bootcamp-launchbase/assets/launchbase.svg",
+        description: "<strong>Domine a programação do zero</strong> e tenha acesso ás melhores oportunidades do mercado",
+        module: "16 módulos",
+        price: "Pago"
+    },
+    {
+        id: "starter",
+        img: "https://storage.googleapis.com/golden-wind/bootcamp-launchbase/assets/starter.svg",
+        description: "<strong>Torne-se um programador desejado</strong> no mercado com esses cursos gratuitos",
+        module: "5 módulos",
+        price: "Gratuito"
     }
 ]
