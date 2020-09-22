ff2fc1c263caad79a90b86c90e7e0b5b1a2c0ba0
José Ulombe <jose.ulombe@hotmail.com>
2020-09-22 20:18:30 +0100

    chore: update configuration

--- modulo3/Desafio 3-3 Página de descriçao do curso/data.js
+++ modulo3/Desafio 3-3 Página de descriçao do curso/data.js
@@ -2,21 +2,21 @@
 module.exports = [
     {
         id: "starter",
-        img: "https://storage.googleapis.com/golden-wind/bootcamp-launchbase/assets/starter.svg",
+        img_url: "https://storage.googleapis.com/golden-wind/bootcamp-launchbase/assets/starter.svg",
         description: "<strong>Torne-se um programador desejado</strong> no mercado com esses cursos gratuitos",
         module: "5 módulos",
         price: "Gratuito"
     },
     {
         id: "launchbase",
-        img: "https://storage.googleapis.com/golden-wind/bootcamp-launchbase/assets/launchbase.svg",
+        img_url: "https://storage.googleapis.com/golden-wind/bootcamp-launchbase/assets/launchbase.svg",
         description: "<strong>Domine a programação do zero</strong> e tenha acesso ás melhores oportunidades do mercado",
         module: "16 módulos",
         price: "Pago"
     },
     {
         id: "gostack",
-        img: "https://storage.googleapis.com/golden-wind/bootcamp-launchbase/assets/gostack.svg",
+        img_url: "https://storage.googleapis.com/golden-wind/bootcamp-launchbase/assets/gostack.svg",
         description: "<strong>Treinamento imersivo</strong> nas tecnologias mais modernas de desenvolvimento web e mobile",
         module: "13 módulos",
         price: "Pago"
