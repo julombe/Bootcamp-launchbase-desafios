d9fe0e37d2f88b6b92e5ab71fcff0e0e4e462d82
José Ulombe <jose.ulombe@hotmail.com>
2020-11-10 23:36:42 +0100

    update routes.js

--- modulo4/routes.js
+++ modulo4/routes.js
@@ -1,3 +1,9 @@
 const express = require('express')
 const routes = express.Router()
 
+// create routes
+routes.get('/', function(req, res) {
+    return res.send("Ok")
+})
+
+module.exports = routes
