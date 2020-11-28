b9455340072faf25bfb5adf57e9e99a6ecc1c082
José Ulombe <jose.ulombe@hotmail.com>
2020-11-28 22:19:43 +0100

    add folder

---
+++ modulo4/Desafio 4-2 - Card do Professor/routes.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const routes = express.Router()
+
+// create routes
+routes.get('/', function(req, res) {
+    return res.redirect("teachers")
+})
+
+routes.get('/teachers', function(req, res) {
+    return res.render("teachers/index")
+})
+
+
+routes.get('/students', function(req, res) {
+    return res.render("students")
+})
+
+module.exports = routes
