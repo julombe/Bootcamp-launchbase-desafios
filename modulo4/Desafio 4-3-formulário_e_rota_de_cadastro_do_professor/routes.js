b1c4261e9fc4260a4b614b8c5e8b3440e1cedcde
José Ulombe <jose.ulombe@hotmail.com>
2020-12-09 20:30:41 +0100

    update routes

--- modulo4/Desafio 4-3-formulário_e_rota_de_cadastro_do_professor/routes.js
+++ modulo4/Desafio 4-3-formulário_e_rota_de_cadastro_do_professor/routes.js
@@ -15,7 +15,7 @@ routes.get('/students', function(req, res) {
     return res.render("students")
 })
 
-routes.get('/teachers', function(req, res) {
+routes.get('/teachers/create', function(req, res) {
     return res.render("teachers/create")
 })
 
