6e4f79e667933dd39aa951cd0a759a5d3df612db
José Ulombe <jose.ulombe@hotmail.com>
2020-12-13 17:41:54 +0100

    update teachers

--- modulo4/Desafio 4-3-formulário_e_rota_de_cadastro_do_professor/teachers.js
+++ modulo4/Desafio 4-3-formulário_e_rota_de_cadastro_do_professor/teachers.js
@@ -1,4 +1,19 @@
-exports.post = function(req, res) {
+exports.post = function(req, res) { 
 
-   
-}
+
+    const keys = Object.keys(req.body)
+
+    for(key of keys)
+    // req.body.key == ""
+    if (req.body[key] == "") {
+        return res.send("Please, fill all fields!")
+    }
+
+
+    return res.send("req.body") 
+})
+
+
+routes.get('/students', function(req, res) {
+    return res.render("students")
+})
