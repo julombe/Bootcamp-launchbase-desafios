0e7f454d9ad481ca7710741d3959fc180f33c6a9
José Ulombe <jose.ulombe@hotmail.com>
2020-12-17 06:57:12 +0100

    update file

--- modulo4/Desafio 4-3-formulário_e_rota_de_cadastro_do_professor/teachers.js
+++ modulo4/Desafio 4-3-formulário_e_rota_de_cadastro_do_professor/teachers.js
@@ -1,20 +1,20 @@
-const fs = require('fs')
+const fs = require("fs");
 
+exports.post = function (req, res) {
+  const keys = Object.keys(req.body);
 
-exports.post = function(req, res) { 
-
-
-    const keys = Object.keys(req.body)
-
-    for(key of keys)
+  for (key of keys) {
     // req.body.key == ""
     if (req.body[key] == "") {
-        return res.send("Please, fill all fields!")
+      return res.send("Please, fill all fields!");
     }
+  }
 
+  fs.writeFile("data.json", JSON.stringify(req.body), function (err) {
+    if (err) return res.send("Write file error!");
 
-    return res.send("req.body") 
-}
+    return res.redirect("/teachers");
+  });
 
-
-
+  //return res.send("req.body")
+};
